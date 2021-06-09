1f2cb0e701c33f602ebf7c02f1d1900974311433
Shadab <akhtarshadab415@gmail.com>
2021-06-09 13:23:20 +0530

    useState useEffect hooks - add delete

--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -1,18 +1,15 @@
 import React from 'react'
+import ContactCard from './ContactCard'
 
 export const ContactList = (props) => {
     console.log(props);
 
+    const deleteContactHandler = (id) =>{
+        props.getContactId(id);
+    }; 
+
     const renderContactList = props.contacts.map((contact) => {
-        return(
-            <div className="item">
-                <div className="content">
-                    <div className="header">{contact.name}</div>
-                    <div>{contact.email}</div>
-                </div>
-                <i className="trash alternate outline icon"></i>
-            </div>
-        );
+        return <ContactCard contact={contact} clickHandler={deleteContactHandler} key={contact.id} />;
 
     }
     );
